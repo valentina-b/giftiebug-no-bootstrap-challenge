7a5286894c01583405b25761922f9b4936193bc6
valentina-b <valentina.bedi@gmail.com>
2021-02-23 20:27:56 +0000

    add burger menu links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 const burgerMenu = document.querySelector('.div-burger-menu');
 const burgerMenuTop = document.querySelector('.top-burger-menu');
+const burgerMenuLinks = document.querySelector('#burger-menu-links');
 
 burgerMenu.addEventListener('click', openBurgerMenu);
 
 function openBurgerMenu() {
     burgerMenuTop.classList.toggle('top-burger-menu-transform');
+    burgerMenuLinks.classList.toggle('display-block-slide');
+    console.log(burgerMenuLinks);
 }
