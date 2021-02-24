73cd44f58fb30861f54ddc1524a65745b00519b5
valentina-b <valentina.bedi@gmail.com>
2021-02-24 18:02:16 +0000

    add modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,40 @@ function closeBurgerMenu() {
     burgerMenuLinks.classList.toggle('display-block-slide');
     burgerMenuTop.classList.toggle('top-burger-menu-transform');
 }
+
+// modal code adjusted from https://www.w3schools.com/howto/howto_css_modals.asp
+
+// Get the modal
+let modal = document.getElementById('modal');
+
+// Get the elements that open the modal
+let ctas = document.getElementsByClassName('cta');
+let modalLinks = document.getElementsByClassName('modal-link');
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the cta/modalLink, open the modal
+for (let cta of ctas) {
+    cta.addEventListener('click', openModal);
+}
+
+for (let modalLink of modalLinks) {
+    modalLink.addEventListener('click', openModal);
+}
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = 'none';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
